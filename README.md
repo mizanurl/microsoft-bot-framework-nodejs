aeb1c09715e4725c5f4968c8ec1cb18d505304d7
Mizanur Islam Laskar <cicakemizan@gmail.com>
2017-09-10 18:13:04 +0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Smart Chatbot w/ Microsoft Bot Framework
+
+A conversational service built on Microsoft's Bot Framework in Node.js.
+
+Demo code used in:
+* Blog post: [How to Build a Chatbot Using Microsoft’s Bot Framework](https://10xnation.com/blog/how-to-build-a-chatbot-using-microsofts-bot-framework/)
---
+++ app.js
@@ -0,0 +1,60 @@
+var builder = require('botbuilder');
+var restify = require('restify');
+var apiairecognizer = require('api-ai-recognizer');
+var request = require('request');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+	console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+	appId: process.env.MICROSOFT_APP_ID,
+	appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
+
+var bot = new builder.UniversalBot(connector);
+
+// Client access token from api.ai
+var recognizer = new apiairecognizer("77eaa15906704947a7f481f4d51f1cf5");
+
+var intents = new builder.IntentDialog({
+	recognizers: [recognizer]
+});
+
+bot.dialog('/',intents);
+
+intents.matches('whatIsWeather',[ function(session,args){
+	var city = builder.EntityRecognizer.findEntity(args.entities,'cities');
+
+	if (city) {
+		var city_name = city.entity;
+		var url = "http://api.apixu.com/v1/current.json?key=7e212e10586f48b2bc313623170909&q=" + city_name;
+
+		request(url,function(error,response,body) {
+			body = JSON.parse(body);
+			//console.log(body);
+			temp = body.current.temp_c;
+			session.send("It's " + temp + " degrees celsius in " + city_name); });
+	} else {
+		builder.Prompts.text(session, 'Which city do you want the weather for?');
+	}
+}, function(session,results){
+	var city_name = results.response;
+	var url = "http://api.apixu.com/v1/current.json?key=7e212e10586f48b2bc313623170909&q=" + city_name;
+	request(url,function(error,response,body) {
+		body = JSON.parse(body);
+		//console.log(body);
+		temp = body.current.temp_c;
+		session.send("It's " + temp + " degrees celsius in " + city_name);
+	});
+} ]);
